2382a2f60532261c87a7d704840a7c1908eb3e12
FurWaz <fur.waz06@gmail.com>
2023-01-18 12:02:27 +0100

    Creation template test JUnit

---
+++ src/zoot/tests/TestConstanteEntiere.java
@@ -0,0 +1,12 @@
+package zoot.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestConstanteEntiere {
+    @Test
+    public void TestConstante() {
+        assertTrue(true);
+    }
+}
