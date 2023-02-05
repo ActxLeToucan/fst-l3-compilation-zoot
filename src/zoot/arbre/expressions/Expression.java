dba6e7e4196ca2b18284c72a6441b270d763e2b6
FurWaz <fur.waz06@gmail.com>
2023-02-05 19:09:49 +0100

    Ajout type a expression

--- src/zoot/arbre/expressions/Expression.java
+++ src/zoot/arbre/expressions/Expression.java
@@ -1,10 +1,21 @@
 package zoot.arbre.expressions;
 
+import zoot.Type;
 import zoot.arbre.ArbreAbstrait;
 
 public abstract class Expression extends ArbreAbstrait {
-    
+    private Type type;
+
     protected Expression(int n) {
         super(n) ;
+    }
+
+    protected Expression(int n, Type t) {
+        super(n) ;
+        this.type = t;
+    }
+
+    public Type getType() {
+        return type;
     }
 }
