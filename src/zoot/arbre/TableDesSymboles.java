496799a2d2bd8bccece373d439ebae481480569d
FurWaz <fur.waz06@gmail.com>
2023-02-27 19:17:26 +0100

    Ajout de blocs a la TDS

--- src/zoot/arbre/TableDesSymboles.java
+++ src/zoot/arbre/TableDesSymboles.java
@@ -3,18 +3,38 @@ package zoot.arbre;
 import zoot.exceptions.DoubleDeclarationException;
 import zoot.exceptions.VariableNonDeclareeException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
 public class TableDesSymboles {
     private static TableDesSymboles instance = null;
-    private final Map<Entree, Symbole> table;
+    private final List<Map<Entree, Symbole>> tables;
 
-    private int positionTete;
+    private final int ELEMENT_SIZE = 4; // taille d'un element (en octets)
 
     private TableDesSymboles() {
-        table = new HashMap<Entree, Symbole>();
-        positionTete = 0;
+        tables = new ArrayList<>();
+        tables.add(new HashMap<>()); // programme principal
+    }
+
+    /**
+     * Retourne la table des symboles du bloc courant
+     * @return table des symboles du bloc courant
+     */
+    private Map<Entree, Symbole> getBlocTable() {
+        return tables.get(tables.size() - 1);
+    }
+
+    /**
+     * Retourne la table des symboles du bloc courant + shift
+     * @param shift décalage par rapport au bloc courant (-1 => un bloc en dessous)
+     * @return table des symboles du bloc courant + shift
+     */
+    private Map<Entree, Symbole> getBlocTable(int shift) {
+        if (shift >= tables.size()) { return null; } // plus de blocs a ce niveau
+        return tables.get(tables.size() - 1 + shift);
     }
 
     public static TableDesSymboles getInstance() {
@@ -25,33 +45,51 @@ public class TableDesSymboles {
     }
 
     public void ajouter(Entree e, Symbole s) throws DoubleDeclarationException {
-        if (table.containsKey(e)) {
+        Map<Entree, Symbole> blocTable = getBlocTable();
+        if (blocTable.containsKey(e)) {
             throw new DoubleDeclarationException(e.toString());
         }
 
-        s.setDeplacement(positionTete); // on deplace le symbole a la position du pointeur (qui est la place libre)
-        positionTete -= 4; // on decale le pointeur de 4 (taille d'un element)
+        // On deplace le symbole a la position du pointeur (qui est la place libre)
+        // = Nombre d'elements deja presents * taille d'un element
+        s.setDeplacement(blocTable.size() * ELEMENT_SIZE);
 
-        table.put(e, s);
+        blocTable.put(e, s);
     }
 
     public Symbole identifier(Entree e) throws VariableNonDeclareeException {
-        if (!table.containsKey(e)) {
-            throw new VariableNonDeclareeException(e.toString());
+        Symbole res = null;
+
+        int level = 0;
+        while (res == null) {
+            Map<Entree, Symbole> blocTable = getBlocTable(level);
+            if (blocTable == null) { break; } // on est sorti du programme principal (pas de blocs en dessous)
+
+            res = blocTable.get(e);
+            level--;
         }
-        return table.get(e);
+
+        if (res == null) { throw new VariableNonDeclareeException(e.toString()); }
+        return res;
+    }
+
+    public void entreeBloc() {
+        tables.add(new HashMap<>()); // on ajoute un nouveau bloc
+    }
+
+    public void sortieBloc() {
+        tables.remove(tables.size() - 1); // on supprime le dernier bloc
     }
 
     /**
      * Retourne la position de la tete de pile (premiere place libre)
-     *
      * @return position (décalage négatif) de la tete de pile par rapport au $sp
      */
     public int getPositionTete() {
-        return positionTete;
+        return getBlocTable().size() * ELEMENT_SIZE;
     }
 
     public int getNbElements() {
-        return table.size();
+        return getBlocTable().size();
     }
 }
