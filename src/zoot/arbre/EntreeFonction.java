2aa9cbfa1f19031f66333b3290570d0edf2a3bc9
Antoine CONTOUX <contoux1u@etu.univ-lorraine.fr>
2023-02-27 19:20:25 +0100

    getNbParametres

--- src/zoot/arbre/EntreeFonction.java
+++ src/zoot/arbre/EntreeFonction.java
@@ -20,6 +20,10 @@ public class EntreeFonction extends Entree {
         return nom + "_" + nbParametres;
     }
 
+    public int getNbParametres() {
+        return nbParametres;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
